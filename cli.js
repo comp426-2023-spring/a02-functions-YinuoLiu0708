1ccfb5f8050c5bd6044cf2ce2ab4530b97a1203f
Yinuo Liu <77820812+YinuoLiu0708@users.noreply.github.com>
2023-02-12 19:39:18 -0500

    complete basic functions

---
+++ cli.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+import minimist from 'minimist';
+var cmd = minimist(process.argv.slice(2));
+
+import moment from 'moment-timezone';
+
+var help = cmd.h || false;
+var latitude = cmd.n || -cmd.s || 38.875;
+var longitude = cmd.e || -cmd.w || -79;
+var timezone = cmd.z || moment.tz.guess();
+var day = (cmd.d !== undefined) ? cmd.d : 1;
+var json = cmd.j || false;
+
+var URL = 'https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=weathercode,precipitation_hours&timezone=' + timezone;
+const response = await fetch(URL);
+const data = await response.json();
+
+function show_help(){
+    console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE");
+    console.log("    -h            Show this help message and exit.");
+    console.log("    -n, -s        Latitude: N positive; S negative.");
+    console.log("    -e, -w        Longitude: E positive; W negative.");
+    console.log("    -z            Time zone: uses tz.guess() from moment-timezone by default.");
+    console.log("    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.");
+    console.log("    -j            Echo pretty JSON from open-meteo API and exit.");
+    process.exit();
+}
+
+function show_json(){
+    console.log(data);
+    process.exit();
+}
+
+function get_weather(){
+    var raining = data.daily.precipitation_hours;
+    var date = get_date();
+    if (raining[day] > 0){
+        console.log("You might need your galoshes " + date);
+    } else {
+        console.log("You will not need your galoshes " + date);
+    }
+    process.exit();
+}
+
+function get_date(){
+    if (day == 0){
+        return "today.";
+    } else if (day == 1){
+        return "tomorrow.";
+    } else {
+        return "in " + day + " days.";
+    }
+}
+
+if (help){
+    show_help();
+} else if (json){
+    show_json();
+} else {
+    get_weather();
+}
